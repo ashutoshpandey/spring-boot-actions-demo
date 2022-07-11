2effeaadb7d1ca3d0e0d8b4afdfde3291d64dd4c
Ashutosh Pandey <87969744+ashutoshpandeydu@users.noreply.github.com>
2022-07-11 17:51:58 +0530

    Added logs

--- src/main/java/com/gitaction/demo/controllers/TestController.java
+++ src/main/java/com/gitaction/demo/controllers/TestController.java
@@ -1,5 +1,7 @@
 package com.gitaction.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +11,23 @@ import com.gitaction.demo.pojo.HelloData;
 
 @RestController
 public class TestController {
+	Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping("/hello")
 	public String sayHello() {
+		logger.trace("Log level: TRACE");
 		return "hello";
 	}
 
 	@RequestMapping("/bye")
 	public String sayBye() {
+		logger.info("Log level: INFO");
 		return "bye";
 	}
 
 	@PostMapping("/bye-to-name")
 	public String sayByeToName(@RequestBody HelloData data) {
+		logger.error("Log level: DEBUG");
 		return "Bye " + data.getName();
 	}
 }
